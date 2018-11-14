1a16937213e11be999197ae215331be4d186936a
kishor.kumar <kishor.kumar@gwmidc.in>
2018-11-14 09:59:12 +0530

    Gflags

---
+++ GFLAGS_Example_3/CMakeFiles/test_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_1.dir/src/main_1.cc.o"
+  "CMakeFiles/test_1.dir/src/main_2.cc.o"
+  "CMakeFiles/test_1.dir/src/test_1.cc.o"
+  "bin/test_1.pdb"
+  "bin/test_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
